fae7f71b052cfa61fdb042d1aab1c14cd12f3125
Franco Loto <franco.loto1995@gmail.com>
2023-04-07 15:25:22 -0300

    ecommerce vue first commit

---
+++ src/views/Success.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="page-success">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">Gracias por tu compra!</h1>
+
+                <p>Su orden será procesada dentro de las próximas 48hs.</p>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Success',
+    mounted() {
+        document.title = 'Gracias por tu compra | Fibra Positiva'
+    },
+}
+</script>
